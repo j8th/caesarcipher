ab3f072f53ab6bc155e56d6d5b16f8737383f1f9
avni510 <avni510@gmail.com>
2016-07-06 13:52:17 -0500

    first attempt at the cipher problem with TDD

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,81 +1,44 @@
-class Encrypt
+class Encrypt 
 
-  def uppercase_letters_in_string(string)
-    uppercase_letters = []
-    (0...string.length).each do |index|
-      uppercase_letters << index if (string[index].ord >= 65 && string[index].ord <= 90)
+  def caesar_cipher(user_input, shift_value)
+    encrypted_string = ""
+    user_input.each_char {|ch| 
+      encrypted_string += encrypt_char(ch, shift_value)
+    }
+    encrypted_string
+  end
+
+  def encrypt_char(char, shift_value)
+    unless letter?(char)
+      return char
     end
-    uppercase_letters
-  end
-
-  def convert_string_to_numeric_rep(string)
-    string = string.downcase
-    character_list = string.split("")
-    numeric_rep_list = []
-    character_list.each do |ch|
-      if (ch.ord >= 0 && ch.ord <= 64) || (ch.ord >= 91 && ch.ord <= 96) || (ch.ord >= 123 && ch.ord <= 126)
-        numeric_rep_list << ch
-      else  
-        numeric_rep_list << ch.ord - "a".ord + 1
-      end
+    shift_value = reindex(shift_value)
+    if upcase?(char)
+      return shift_letter(char, shift_value, "A")
+    else
+      return shift_letter(char, shift_value, "a")
     end
-    numeric_rep_list
   end
 
-  def shift(numeric_list, shift_value)
-    shifted_list = []
-    numeric_list.each do |item|
-      if item.is_a? String
-        shifted_list << item 
-      else  
-        sum = item + shift_value
-        while sum > 26 do
-          sum -= 26
-        end
-        shifted_list << sum
-      end
-    end
-    shifted_list
+  def reindex(value)
+    value % 26
   end
 
-  def convert_numeric_rep_to_alpha(numeric_list)
-    letter_list = []
-    numeric_list.each do |item|
-      if item.is_a? String
-        letter_list << item 
-      else
-        ascii_rep = 96 + item
-        letter_list << ascii_rep.chr
-      end
-    end
-    letter_list
+  def shift_letter(char, shift_value, start_ascii)
+    encrypt_value = (char.ord - start_ascii.ord) + shift_value
+    encrypt_value -= 26 if encrypt_value > 26
+    return (encrypt_value + start_ascii.ord).chr
   end
 
-  def convert_letters_to_uppercase(alpha_list, uppercase_index_list)
-    # return alpha_list if uppercase_index_list == []
-    # should this method have error checking? ex: if the specified index is larger than the length of the string
-    # or the specified index is a special character 
-    for item in uppercase_index_list 
-      alpha_list[item].upcase!
-    end
-    alpha_list
+  def upcase?(char)
+    uppercase_range = ("A".."Z")
+    uppercase_range.include?(char)
   end
 
-  def encrypt_input(string, shift_value)
-    uppercase_letters_list = uppercase_letters_in_string(string)
-    number_rep_list = convert_string_to_numeric_rep(string)
-    number_rep_list_shifted = shift(number_rep_list, shift_value)
-    encrypted_list = convert_numeric_rep_to_alpha(number_rep_list_shifted)
-    encrypted_list = convert_letters_to_uppercase(encrypted_list, uppercase_letters_list)
-    encrypted_list.join("")
-  end
-
-  def run
-    puts "What phrase would you like to encrypt?"
-    user_input_string = gets.chomp
-    puts "What value would you like it to shift by?"
-    user_input_shift_value = gets.chomp
-    encrypt_input(user_input_string, user_input_shift_value)
+  def letter?(char)
+    uppercase_range = ("A".."Z")
+    lowercase_range = ("a".."z")
+    uppercase_range.include?(char) || lowercase_range.include?(char)
   end
 
 
--- spec/encrypt_spec.rb
+++ spec/encrypt_spec.rb
@@ -1,91 +1,78 @@
 require "encrypt"
 
-describe "Encrypt" do 
+describe Encrypt do
 
-  let(:encrypt) { Encrypt.new }
+  let(:encrypt){ Encrypt.new }
 
-  describe ".uppercase_letters_in_string" do
+  #create more then one test case
 
-    inputs_and_outputs = { "Hello World" => [0, 6], "What a string!" => [0], "hello world" => []}
-    
-    it "returns a list of the uppercase letters in the string" do 
-      inputs_and_outputs.each do |input, output|
-        expect(encrypt.uppercase_letters_in_string(input)).to eq(output)
-      end
-    end
-  end
+  # it "returns the same character if the shift value is 0" do 
 
-  describe ".convert_string_to_numeric_rep" do 
+  #   inputs_and_outputs = { ["A", 0] => "A", ["Q", 0] => "Q"}
 
-    it "returns a list of each letter in the string converted to it's alphabetical number" do
-  
-      inputs_and_outputs = {"Hello World" => [8, 5, 12, 12, 15, " ", 23, 15, 18, 12, 4], "stRing!" => [19, 20, 18, 9, 14, 7, "!"], "..." => [".", ".", "."]}
-      
-      inputs_and_outputs.each do |input, output|
-        expect(encrypt.convert_string_to_numeric_rep(input)).to eq(output)
-      end
-    end
-  end
-
-  describe ".shift" do 
-
-    it "returns a list of numbers increased by the shift value that correspond to their place in the alpahbet" do
-
-      inputs_and_outputs = {[[8, 5, 12, 12, 15, " ", 23, 15, 18, 12, 4], 10] => [18, 15, 22, 22, 25, " ", 7, 25, 2, 22, 14], [[19, 20, 6, "!"], 35] => [2, 3, 15, "!"], [[".", ".", "."], 5] => [".", ".", "."]}
-
-      inputs_and_outputs.each do |input, output|
-        expect(encrypt.shift(input[0], input[1])).to eq(output)
-      end
-    end
-  end
-
-  describe ".convert_numeric_rep_to_alpha" do 
-
-    it "converts a list of numbers (1 to 26) to its corresponding alphabetical character" do 
-      
-      inputs_and_outputs = {[18, 15, 22, 22, 25, " ", 7, 25, 2, 22, 14] => ["r", "o", "v", "v", "y", " ", "g", "y", "b", "v", "n"], [2, 3, 15, "!"] => ["b", "c", "o", "!"], [".", ".", "."] => [".", ".", "."]}
-    
-      inputs_and_outputs.each do |input, output|
-        expect(encrypt.convert_numeric_rep_to_alpha(input)).to eq(output)
-      end
-    end
-  end
-
-  describe ".convert_letters_to_uppercase" do 
-
-    it "converts specified lowercase letters into uppercase letters" do 
-
-      inputs_and_outputs = {[ ["r", "o", "v", "v", "y", " ", "g", "y", "b", "v", "n"], [0, 7] ] => ["R", "o", "v", "v", "y", " ", "g", "Y", "b", "v", "n"], [ ["b", "c", "o", "!"], [0, 1, 2] ] => ["B", "C", "O", "!"], [ ["b", "c", "o", "!"], [] ] => ["b", "c", "o", "!"] }
-
-      inputs_and_outputs.each do |input, output|
-        expect(encrypt.convert_letters_to_uppercase(input[0], input[1])).to eq(output)
-      end
-    end
-  end
-
-  describe ".encrypt_input" do 
-
-    it "encrypts a string" do 
-      
-      inputs_and_outputs = {["What a string!", 5] => "Bmfy f xywnsl!", ["Hello$ World", 13] => "Uryyb$ Jbeyq", ["...", 4] => "...", ["538", 5] => "538"}
-
-      inputs_and_outputs.each do |input, output|
-        expect(encrypt.encrypt_input(input[0], input[1])).to eq(output)
-      end
-    end
-  end
-
-  # describe ".run" do
-  #   let(:user_string) { "What a string!"}
-  #   let(:user_shift_value) { 5 }
-
-  #   it "returns an encrypted version of the users input" do 
-  #     encrypt.stub(:gets) {"What a string!"}
-  #     expect(encrypt.run).to eq("What a string!")
-  #     # encrypt.run.stub(:gets).and_return("What a string!")
+  #   inputs_and_outputs.each do |input, output|
+  #     expected = output
+  #     actual = encrypt.caesar_cipher(input[0], input[1])
+  #     expect(actual).to eq(expected)
   #   end
   # end
 
-  
+  it "returns the encrypted letter based on the shift value" do 
+
+    inputs_and_outputs = { ["A", 4] => "E", ["B", 2] => "D", ["A", 0] => "A", 
+      ["a", 1] => "b", ["b", 3] => "e"}
+
+    inputs_and_outputs.each do |input, output|
+      expected = output
+      actual = encrypt.caesar_cipher(input[0], input[1])
+      expect(actual).to eq(expected)
+    end
+  end
+
+  it "returns the same value if the character is a special character" do 
+
+    inputs_and_outputs = { ["!", 5] => "!", ["$", 7] => "$"}
+
+    inputs_and_outputs.each do |input, output|
+      expected = output
+      actual = encrypt.caesar_cipher(input[0], input[1])
+      expect(actual).to eq(expected)
+    end
+  end
+
+  # it "returns a lowercase encrypted letter based on the shift value if user input is lowercase" do 
+    
+  #   inputs_and_outputs = { ["a", 1] => "b", ["b", 3] => "e"}
+
+  #   inputs_and_outputs.each do |input, output|
+  #     expected = output
+  #     actual = encrypt.caesar_cipher(input[0], input[1])
+  #     expect(actual).to eq(expected)
+  #   end
+  # end
+
+  it "returns an encrypted letter by a shift value from 1 to 26" do 
+
+    expected = "B"
+    actual = encrypt.caesar_cipher("A", 27)
+    expect(actual).to eq(expected)
+  end
+
+  it "returns an encrypted letter that wraps around the alphabet" do 
+
+    inputs_and_outputs = { ["z", 3] => 'c', ["Y", 5] => "D" }
+
+    inputs_and_outputs.each do |input, output|
+      expected = output
+      actual = encrypt.caesar_cipher(input[0], input[1])
+      expect(actual).to eq(expected)
+    end
+  end
+
+  it "encrypts a string of characters" do 
+    expected = "BMfy. f xywnsl!"
+    actual = encrypt.caesar_cipher("WHat. a string!" , 5)
+    expect(actual).to eq(expected)
+  end
 
 end
